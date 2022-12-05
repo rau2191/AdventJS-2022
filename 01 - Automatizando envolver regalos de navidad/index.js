c05da627d8e97132f7709966ce60512b329c152a
Raul Maestre <rau2191@hotmail.com>
2022-12-05 16:01:33 +0100

    Wrapping with regExp

--- 01 - Automatizando envolver regalos de navidad/index.js
+++ 01 - Automatizando envolver regalos de navidad/index.js
@@ -7,11 +7,19 @@ function wrapping(gifts) {
 }
 
 function wrapping2(gifts) {
-    return gifts.map(gift =>  ` \n*${gift}*\n `.replaceAll(" ", "*".repeat(gift.length + 2)))
+    return gifts
+        .map(gift => ["", `\n*${gift}*\n`, ""].join("*".repeat(gift.length + 2)))
 }
+
+function wrapping3(gifts) {
+    return gifts
+        .map(gift => gift.replace(/([a-zA-Z]{1,})/,"*".repeat(gift.length + 2)+"\n*$1*\n"+"*".repeat(gift.length + 2)))
+  }
 
 const gifts = ['book', 'game', 'socks']
 const wrapped = wrapping(gifts)
 const wrapped2 = wrapping2(gifts)
+const wrapped3 = wrapping2(gifts)
 console.log(wrapped)
 console.log(wrapped2)
+console.log(wrapped3)
