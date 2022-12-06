246fba99e6722ec359d91349bc9f0eb87b7645c3
Raul Maestre <rau2191@hotmail.com>
2022-12-07 00:32:30 +0100

    Add differents solutions

--- 06 - Creando adornos navideños/index.js
+++ 06 - Creando adornos navideños/index.js
@@ -10,6 +10,18 @@ function createCube(size) {
 }
 
 function createCube2(size) {
+    const createCubeNode = (val, isTop) => {
+        return isTop
+            ? " ".repeat(size - val - 1) + "/\\".repeat(val + 1) + "_\\".repeat(size)
+            : " ".repeat(val - size) + "\\/".repeat(size - (val - size)) + "_/".repeat(size)
+    }
+
+    return Array.from(Array(size * 2).keys())
+        .map((_, index) => createCubeNode(index, ((size * 2) / (index + 1)) > (size / 2)))
+        .join("\n")
+}
+
+function createCube3(size) {
     return Array.from(Array(size).keys())
         .reduce((cube, val) => {
             cube[val] = " ".repeat(size - val - 1) + "/\\".repeat(val + 1) + "_\\".repeat(size)
@@ -20,5 +32,17 @@ function createCube2(size) {
         .join("\n");
 }
 
+function createCube4(size) {
+    let top = '';
+    let bottom = '';
+    for (let i = 1; i <= size; i++) {
+        top += ' '.repeat(size - i) + '/\\'.repeat(i) + '_\\'.repeat(size) + '\n';
+        bottom += ' '.repeat(i - 1) + '\\/'.repeat(size - i + 1) + '_/'.repeat(size) + (i !== size ? "\n" : "");
+    }
+    return top + bottom;
+}
+
 console.log(createCube(3))
 console.log(createCube2(3))
+console.log(createCube3(3))
+console.log(createCube4(3))
