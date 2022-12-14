ce12211e52387658f94ce567611db86b908581ce
Raul Maestre <rau2191@hotmail.com>
2022-12-14 13:17:18 +0100

    Improve internal function equals()

--- 10 - El salto del trineo de Papá Noel/index.js
+++ 10 - El salto del trineo de Papá Noel/index.js
@@ -2,15 +2,18 @@
   Gaussian bell peak is the max value and the values of both sides must go decreasing
 */
 function checkJump(heights) {
+   const equals = (arr1, arr2) => 
+    arr1.every((h, index) => h === arr2[index]);
+  
   const indexOfMaxValue = heights.indexOf(Math.max(...heights));
 
-  const left = heights.slice(0, indexOfMaxValue)
-  const right = heights.slice(indexOfMaxValue + 1)
+  const left = heights.slice(0, indexOfMaxValue);
+  const right = heights.slice(indexOfMaxValue + 1);
 
   return left.length > 0
     && right.length > 0
-    && JSON.stringify(left) === JSON.stringify(left.sort())
-    && JSON.stringify(right) === JSON.stringify(right.sort((a, b) => b - a))
+    && equals(left, [...left].sort())
+    && equals(right, [...right].sort((a, b) => b - a));
 }
 
 
