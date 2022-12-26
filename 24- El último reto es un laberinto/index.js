6110b10d3549b121b5c1254a2b4dc11c1b4c51a4
Raul Maestre <rau2191@hotmail.com>
2022-12-26 21:46:53 +0100

    Challenge 24 resolved

---
+++ 24- El último reto es un laberinto/index.js
@@ -0,0 +1,59 @@
+function canExit(maze) {
+    const ENTRANCE = "S";
+    const EXIT = "E";
+    const WALL = "W";
+
+    const entranceRow = maze.findIndex(row => row.includes(ENTRANCE));
+    const entranceCol = maze[entranceRow].indexOf(ENTRANCE);
+    
+    const visitedCells = [];
+    const nextMoves = [[entranceRow, entranceCol]];
+    const possibleMovements = [
+        [0, -1],
+        [0, 1],
+        [-1, 0],
+        [1, 0]
+    ]
+    const validations = [
+        (row, col) => row < 0 || row >= maze.length || col < 0 || col >= maze[0].length,
+        (row, col) => visitedCells.some(c => c[0] === row && c[1] === col),
+        (row, col) => maze[row][col] === WALL
+    ]
+
+    const findExit = () => {
+        if (nextMoves.length === 0) return false;
+        const [currentY, currentX] = nextMoves.shift();
+        if (maze[currentY][currentX] === EXIT) return true;
+
+        possibleMovements.forEach(([movY, movX]) => {
+            const row = movY + currentY;
+            const col = movX + currentX;
+
+            if (validations.every(validation => !validation(row, col))) {
+                visitedCells.push([row, col]);
+                nextMoves.push([row, col]);
+            }
+        });
+
+        return findExit();
+    }
+
+    return findExit();
+}
+
+
+console.log("Expected:", true, "Actual:", canExit([
+    [' ', ' ', 'W', ' ', 'S'],
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', 'W', ' '],
+    ['W', 'W', ' ', 'W', 'W'],
+    [' ', ' ', ' ', ' ', 'E']
+]));
+
+console.log("Expected:", false, "Actual:", canExit([
+    [' ', ' ', 'W', 'W', 'S'],
+    [' ', ' ', ' ', 'W', ' '],
+    [' ', ' ', ' ', 'W', ' '],
+    ['W', 'W', ' ', 'W', 'W'],
+    [' ', ' ', ' ', ' ', 'E']
+]));
